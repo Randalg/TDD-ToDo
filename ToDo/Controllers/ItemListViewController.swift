ed41dc210fc194bcfed26a7cfd9222c22b99ed28
MMG <martin.gyupchanov@quickseries.com>
2017-12-04 13:08:59 -0500

    Rename the test folder from New Folder to Model

---
+++ ToDo/Controllers/ItemListViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ItemListViewController,ViewController.swift
+//  ToDo
+//
+//  Created by MMG on 2017-12-04.
+//  Copyright © 2017 Martin Gyupchanov. All rights reserved.
+//
+
+import UIKit
+
+class ItemListViewController: UIViewController {
+  var tableView: UITableView?
+  
+  override func viewDidLoad() {
+    tableView = UITableView()
+  }
+}
---
+++ ToDoTests/Controllers/ItemListViewControllerTests.swift
@@ -0,0 +1,30 @@
+//
+//  ItemListViewControllerTests.swift
+//  ToDoTests
+//
+//  Created by MMG on 2017-12-04.
+//  Copyright © 2017 Martin Gyupchanov. All rights reserved.
+//
+
+import XCTest
+@testable import ToDo
+
+class ItemListViewControllerTests: XCTestCase {
+  
+  override func setUp() {
+    super.setUp()
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+  
+  override func tearDown() {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    super.tearDown()
+  }
+  
+  func test_TableViewIsNotNilAfterViewDidLoad() {
+    let sut = ItemListViewController()
+    _ = sut.view
+    
+    XCTAssertNotNil(sut.tableView)
+  }
+}
